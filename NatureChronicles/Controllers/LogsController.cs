e26f859cd2513f6315e5a75fed6b53a10168b4e9
Alican Ceylan <alicanceylan8@outlook.com>
2020-11-11 21:18:30 -0500

    Add project files.

---
+++ NatureChronicles/Controllers/LogsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NatureChronicles.Controllers
+{
+    public class LogsController : Controller
+    {
+        public IActionResult LogHome()
+        {
+            return View();
+        }
+    }
+}
